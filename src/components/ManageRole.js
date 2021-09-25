ef7fc30d42ce322efab8c264d5c9e72dc22c1988
mirza irtiza <mirzairtiza51@gmail.com>
2021-09-26 01:54:56 +0500

    post method

--- src/components/ManageRole.js
+++ src/components/ManageRole.js
@@ -1,31 +1,29 @@
 
-import { useState, useEffect } from 'react'
-import axios from "axios"
+import { useState } from 'react'
 import ToggleSwitch from './ToggleSwitch'
 import Pagination from './Pagination'
+import { Link } from 'react-router-dom'
 const ManageRole = ({ data}) => {
     const [ active, setActive ] = useState(true);
- 
+    const checkedHandler = ()=>{
+        
+    }
     return (
         <>
-                <div className="flex flex-col" style={{height: "84vh"}}>
-
-                
-                    <div className="inner-head flex justify-between items-center ">
+                <div className="flex flex-col h-full">        
+                    <div className="inner-head flex justify-between items-center mb-7">
                         <h2 className="text-blue-600 text-3xl">Manage Role</h2>
                         <div className="button-wrapper flex flex-row">
                             {/* <button className="px-9 py-2 border-2 border-indigo-500 text-blue-600 rounded-md mx-3">Filter</button> */}
-                            <a href="/addrole" className="px-9 py-2 bg-blue-800 border-2 border-indigo-500 text-white rounded-md"
+                            <Link to="/addrole" className="px-9 py-2 bg-cus-green border-2 border-indigo-500 text-white rounded-md"
                            
                             >Add New Role
                             
-                            </a>
+                            </Link>
                         </div>
                     </div>
-
-                    {/*  */}
-                  
-                    <table class="rwd-table w-full border-2 border-gray-200">
+                    <div className="table-scroller border-b-2 border-gray-200">
+                    <table className="rwd-table w-full border-2 border-gray-200">
                         <tbody>
                         <tr className="bg-gray-200">
                             <th className=" py-3 px-3 text-left">Role Name</th>
@@ -34,34 +32,29 @@ const ManageRole = ({ data}) => {
                             <th className=" py-3 px-3 text-left" colSpan="2">Created</th>
                         </tr>
                         {
-                            data.map((result)=>{
+                            data.reverse().map((result)=>{
                             return( 
-                                <>
                                     <tr className="" key={result.id}>
                                 <td className=" py-3 px-3 text-left" data-th="Role Name">{result.name}</td>
                                 <td className=" py-3 px-3 text-left" data-th="Role Name">{result.discription}</td>
                                 <td className=" py-3 px-3 text-left" data-th="Discription">
                                     {/* toggle switch */}
-                                    <ToggleSwitch id="toggle" optionLabels=""  checked={ result.status } onChange={ (checked)=> setActive(checked) } />
+                                    <ToggleSwitch id="toggle"   checked={ result.status.value } onChange={ (checked)=> setActive(checked) } />
                                 </td>
                                 <td className=" py-3 px-3 text-left" data-th="Created">{result.created}</td>
-                                <td className=" py-3 px-3 text-left"> <button className="text-blue-800">Edit</button> </td>
+                                <td className=" py-3 px-3 text-left"> <Link to="/addrole" className="text-blue-800">Edit</Link> </td>
 
                                 </tr>
-                                </>
+                              
                                 )
                                 })
                             }
                         </tbody>
                     </table>
-
-
-                        
                     
+                    </div>
                     {/* Table start */}
-               
-
-                <Pagination />
+                    <Pagination />
                 </div>
 
         </>
